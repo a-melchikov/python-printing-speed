412186f0277239d793a6fc6ffe34775217b32e7e
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-06-01 23:26:48 +0400

    The first working version. Characters are underlined in red if they are written incorrectly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter import ttk
 import time
 
 
@@ -8,48 +7,74 @@ def end():
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    matches = sum(
-        char_first == char_second for char_first, char_second in zip(entry.get(), TEXT)
-    )
+    accuracy = (((len(TEXT) - len(mismatches)) / len(TEXT)) * 100)
+    error_count = len(mismatches)
+    total_chars = len(TEXT)
+    speed = len(TEXT) * (60 / elapsed_time)
 
-    print(f"Аккуратность {((matches / len(TEXT)) * 100):.2f}%")
-    print(f"Заняло времени {elapsed_time:.2f} секунд")
-    print(f"Скорость {len(TEXT) * (60 / elapsed_time):.2f} символов в минуту")
+    print(f"Аккуратность: {accuracy:.2f}%")
+    print(f"Ошибок {error_count} из {total_chars} символов")
+    print(f"Заняло времени {elapsed_time:.2f} сек.")
+    print(f"Скорость {speed:.2f} символов в минуту")
+
     root.destroy()
 
 
 def on_key_press(event):
     global start_time
+
     if start_time is None:
         start_time = time.time()
+
+    if event.keysym == "Return":
+        return "break"
 
 
 def on_key_release(event):
     global start_time
-    if entry.get() == "":
+    global mismatches
+
+    if event.keysym == "Return":
+        return "break"
+
+    text = text_widget.get("1.0", END).strip()
+    idx = len(text) - 1
+
+    if 0 <= idx < len(TEXT):
+        if text[idx] != TEXT[idx]:
+            mismatches.add(idx)
+            text_widget.tag_add("mistake", f"1.{idx}", f"1.{idx+1}")
+        else:
+            text_widget.tag_remove("mistake", f"1.{idx}", f"1.{idx+1}")
+
+    if text == "":
         start_time = None
-
-
-def on_enter(event):
-    end()
+        mismatches.clear()
+        text_widget.tag_remove("mistake", "1.0", END)
+    elif text == TEXT:
+        end()
 
 
 TEXT = "hello world"
 
 root = Tk()
 root.title("Typing speed")
-root.geometry("250x150")
+root.geometry("400x200")
 
 start_time = None
 end_time = None
+mismatches = set()
 
-label = ttk.Label(text=TEXT)
-label.pack(anchor="center")
+display_widget = Text(root, height=1, wrap="none", font=("Helvetica", 18))
+display_widget.insert("1.0", TEXT)
+display_widget.configure(state=DISABLED)
+display_widget.pack(anchor="nw")
 
-entry = ttk.Entry()
-entry.pack(expand=True)
-entry.bind("<KeyPress>", on_key_press)
-entry.bind("<KeyRelease>", on_key_release)
-entry.bind("<Return>", on_enter)
+text_widget = Text(root, font=("Helvetica", 18))
+text_widget.pack(expand=True)
+text_widget.bind("<KeyPress>", on_key_press)
+text_widget.bind("<KeyRelease>", on_key_release)
+
+text_widget.tag_configure("mistake", underline=True, foreground="red")
 
 root.mainloop()
